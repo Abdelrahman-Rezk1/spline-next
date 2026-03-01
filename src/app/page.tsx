fd2955cd361d8f63d3af7b6061d480b304e739a6
Abdelrahman-Rezk1 <abdelrahman.rezk.elmohamady@outlook.com>
2026-03-01 14:17:58 +0200

    update url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
     <main style={{ height: '100vh' }}>
-      <Spline scene="https://prod.spline.design/8IGDlNkB15m3mO5K/scene.splinecode" />
+      <Spline scene="/spline/scene.splinecode" />
     </main>
   );
 }
